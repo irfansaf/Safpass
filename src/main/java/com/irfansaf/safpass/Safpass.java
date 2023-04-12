60f1f26d7ad6b923a13740c08936a74a5e3cc3ed
Irfan Saf <irfansaf7@gmail.com>
2023-04-12 12:35:34 +0700

    Adding FlatLaf UI Components

---
+++ src/main/java/com/irfansaf/safpass/Safpass.java
@@ -0,0 +1,12 @@
+package com.irfansaf.safpass;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLaf;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class Safpass {
+    public static void main(String[] args) {
+
+    }
+}
