3491b6f3a116990a41eac09c0e967034f66102f1
Irfan Saf <irfansaf7@gmail.com>
2023-04-20 00:55:43 +0700

    Adding DateUtils

---
+++ src/main/java/com/irfansaf/safpass/util/DateUtils.java
@@ -0,0 +1,47 @@
+package com.irfansaf.safpass.util;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class DateUtils {
+    private static final Logger LOG = Logger.getLogger(DateUtils.class.getName());
+
+    private DateUtils() {
+        // Utility Class
+    }
+
+    public static DateTimeFormatter createFormatter(String format) {
+        DateTimeFormatter formatter;
+        try {
+            formatter = DateTimeFormatter.ofPattern(format);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            LOG.log(Level.WARNING, String.format("Could not parse date format [%s] due to [%s]", format, e.getMessage()));
+            formatter = DateTimeFormatter.ISO_DATE;
+        }
+        return formatter;
+    }
+
+    public static String formatIsoDateTime(String dateString, DateTimeFormatter formatter) {
+        LocalDateTime dateTime;
+        try {
+            dateTime = LocalDateTime.parse(dateString, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        } catch (DateTimeParseException | NullPointerException e) {
+            try {
+                // Fallback to epoch timestamp
+                Date date = new Date(Long.parseLong(dateString));
+                dateTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+            } catch (NumberFormatException | DateTimeParseException | NullPointerException ex) {
+                LOG.log(Level.WARNING, String.format("Could not parse timestamp [%s] due to [%s]", dateString, ex.getMessage()));
+                dateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(0), ZoneId.systemDefault());
+            }
+        }
+        return formatter.format(dateTime.truncatedTo(ChronoUnit.SECONDS));
+    }
+}
