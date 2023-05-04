8485296b0007df41f38ea26ab6f9363d42246c70
Irfan Saf <irfansaf7@gmail.com>
2023-05-04 08:08:40 +0700

    Fix the 404 Error when validate

--- src/main/java/com/irfansaf/safpass/ui/PurchaseCodeDialog.java
+++ src/main/java/com/irfansaf/safpass/ui/PurchaseCodeDialog.java
@@ -1,5 +1,7 @@
 package com.irfansaf.safpass.ui;
 
+import com.formdev.flatlaf.extras.components.FlatButton;
+import com.formdev.flatlaf.extras.components.FlatTextField;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -8,18 +10,23 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.prefs.BackingStoreException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.irfansaf.safpass.Safpass;
 import com.irfansaf.safpass.util.Configuration;
 
 
 public class PurchaseCodeDialog extends JDialog {
-    private JTextField purchaseCodeField;
+    private static final Logger LOG = Logger.getLogger(PurchaseCodeDialog.class.getName());
+
+    private final FlatTextField purchaseCodeField = new FlatTextField();
     private JLabel messageLabel;
-    private JButton submitButton;
+    private final FlatButton submitButton = new FlatButton();
 
     public PurchaseCodeDialog() {
         setTitle("Enter Purchase Code");
@@ -27,9 +34,9 @@ public class PurchaseCodeDialog extends JDialog {
         setLayout(new BorderLayout());
         setModal(true);
 
-        purchaseCodeField = new JTextField(16);
+        purchaseCodeField.setColumns(16);
         messageLabel = new JLabel("Please Enter your purchase code:");
-        submitButton = new JButton("Submit");
+        submitButton.setText("Submit");
 
         JPanel centerPanel = new JPanel();
         centerPanel.add(purchaseCodeField);
@@ -54,11 +61,24 @@ public class PurchaseCodeDialog extends JDialog {
                 }
 
                 try {
-                    URL url = new URL("http://localhost/api/validate-purchase-code/" + purchaseCode);
+                    URL url = new URL("http://127.0.0.1:8000/api/validate-purchase-code");
+                    LOG.log(Level.INFO, "Request URL: {0}", url.toString());
                     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                     connection.setRequestMethod("POST");
+                    connection.setRequestProperty("Content-Type", "application/json");
+                    connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+                    connection.setDoOutput(true);
+
+                    String requestBody = String.format("{\"purchase_code\": \"%s\"}", purchaseCode);
+                    try (OutputStream os = connection.getOutputStream()) {
+                        byte[] input = requestBody.getBytes("utf-8");
+                        os.write(input, 0, input.length);
+                    }
 
                     int responseCode = connection.getResponseCode();
+
+                    LOG.log(Level.INFO, "Status code: {0}", responseCode);
+
                     if (responseCode == HttpURLConnection.HTTP_OK) {
                         Configuration.getInstance().savePurchaseCode(purchaseCode);
                         messageLabel.setText("Purchase code validated successfully.");
@@ -67,6 +87,7 @@ public class PurchaseCodeDialog extends JDialog {
                         messageLabel.setText("Invalid purchase code. Please try again.");
                     }
                 } catch (IOException | BackingStoreException ex) {
+                    LOG.log(Level.SEVERE, "Error validating the purchase code. Please try again", e);
                     messageLabel.setText("Error validating the purchase code. Please try again");
                 }
             }
